fce97205f95acaf04b6f4b6d74f72a1dfd3e20d1
Sharjeel Zubair <szubair.ka@emaar.ae>
2018-10-14 16:52:34 +0400

    Update

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <title>Hospital System Admin</title>
+        <title>LARATRUST CMS</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
