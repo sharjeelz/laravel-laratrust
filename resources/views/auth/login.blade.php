178919d949ae353a090e4ec2945d7ce1413a0c04
Sharjeel Zubair <szubair.ka@emaar.ae>
2018-10-18 09:17:25 +0400

    Header Change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 
 <head>
-    <title>Hospital System Admin</title>
+    <title>LARATRUST CMS</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
