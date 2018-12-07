18b2109bb1073b805a17ce61ffc7f0143e9fbc7a
Sharjeel Zubair <szubair.ka@emaar.ae>
2018-12-07 16:59:29 +0400

    add token on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link href="{{asset('css/maince5a.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('css/fonts.css')}}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="{{asset('css/app.css')}}" crossorigin="anonymous">
